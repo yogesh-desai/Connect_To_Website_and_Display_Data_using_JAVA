04797cda590a1482569566c3206a76e98d5a6b58
Yogesh Desai <yogesh-desai@users.noreply.github.com>
2016-01-14 18:07:45 +0530

    Update WebDisplay_Data.java

--- WebDisplay_Data.java
+++ WebDisplay_Data.java
@@ -1,5 +1,8 @@
-// to connect to a website & display the data of a web page
-// JAVA provides "java.net" package which contains classes required for network programming.
+// Title: To connect to a Website & Display the data of a Web Page
+/**
+ *
+ * @author YogeshD
+ */
 import java.net.*;
 import java.io.*;
 
@@ -7,21 +10,24 @@ class WebDisplay_Data
 {
 	public static void main(String args[ ]) throws MalformedURLException, IOException
 	{
+		// Step 1: Create Buffered Reader Object To read Input from user
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		// Step 2: Read the input from the user
 		System.out.print("Enter the url of website :- ");
 		String url = br.readLine();
-		// now open the connection to the web site by calling
-		// openConnection() of URL object
+		
+		// Step 3: Now open the connection to the web site by calling openConnection() of URL object
 		URL url_adr = new URL(url);
 		URLConnection urlc = url_adr.openConnection();
-
+		
+		// Additional check to prevent un-necessary execution. Check the length of URL Object
 		int len = urlc.getContentLength();
 		if (len == 0)
 			System.out.println("Resource is not available");
 		else
 		{
-			// fetch the contents of the resource by using
-			// an I/P stream
+			// Step 4: Fetch the contents of the resource by using an input stream
 			InputStream ip = urlc.getInputStream();
 			System.out.println("Contents are :- ");
 			while (true)
@@ -29,7 +35,7 @@ class WebDisplay_Data
 				int no = ip.read();
 				if (no == -1)
 					break;
-
+				// Step 5: Print the data. You can store it to specific location if you want.
 				System.out.print((char) no);
 			}
 			ip.close();
